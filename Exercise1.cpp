b0cfb562f45d62ef5358ba963a06d94028275914
ldt2711 <ldt2711@gmail.com>
2024-10-17 23:12:16 +0700

    Added class node to ex1.cpp

--- Exercise1.cpp
+++ Exercise1.cpp
@@ -1,2 +1,23 @@
 #include <iostream>
 using namespace std;
+
+template<class T>
+class node
+{
+private:
+	T elem;
+	node *next, *prev;
+public:
+	node(T e=0, node<T> *P=0, node<T> *N=0)
+	{
+		elem = e;
+		next = N;
+		prev = P;
+	}
+	void setElem(T e) {elem = e;}
+	void setNext(node<T> *N) {next = N;}
+	void setPrev(node<T> *P) {prev = P;}
+	T getElem() {return elem;}
+	node<T> *getNext() {return next;}
+	node<T> *getPrev() {return prev;}
+};
